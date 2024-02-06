81ada1908246052db80ed1666abc1fc01c2567fd
Theeicon45 <157601461+Theeicon45@users.noreply.github.com>
2024-02-06 21:56:24 +0300

    jhjh

--- dashboard.php
+++ dashboard.php
@@ -1,65 +1,32 @@
+<?php
+
+    include 'database/db_dashboard.php';
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>dashboard</title>
     <link rel="stylesheet" href="styles/dashboard.css">
 </head>
 <body>
-    <div class="back">
-        <div class="txt">Welcome, </div>
+    <nav>
+        <div class="title">WELCOME <?php echo $userDetails['first_name'];?></div>
+    </nav>
 
-        <container>
-            <div class="recieved_item">
-                <img src="images/recieve.png" class="rec_img" alt="recieved image">
-                <div class="txt1">RECEIVED</div>
-                <table >
-                    <tr>
-                        <th>Pending</th>
-                        <th>Received</th>
-                        <th>Total</th>
-                    </tr>
-                    <tr>
-                        <td></td>
-                    </tr>
-                    
-                </table>
-            </div>
-
-            <div class="sent_item">
-            <img src="images/send.svg" class="rec_img" alt="recieved image">
-                <div class="txt1">SENT</div>
-                <table >
-                    <tr>
-                        <th>Pending</th>
-                        <th>Sent</th>
-                        <th>Total</th>
-                    </tr>
-                    <tr>
-                        <td></td>
-                    </tr>
-                    
-                </table>
-            </div>
-        </container>
-
-        <div class="contain">
-            <div class="pending">
-            <table >
-                    <tr>
-                        <th>Date</th>
-                        <th>Sender</th>
-                        <th>Item No</th>
-                        <th>Received</th>
-                    </tr>
-                    <tr>
-                        <td></td>
-                    </tr>
-                    
-                </table>
-            </div>
+    <container>
+        <div class="details">
+        <img src="images/male avator.png" alt="an avatar">
+            <div class="name">First Name: <?php echo $userDetails['first_name']; ?></div>
+            <div class="name">Last Name: <?php echo $userDetails['second_name']; ?></div>
+            <div class="name">Email: <?php echo $userDetails['email']; ?></div>
+            <div class="name">County: <?php echo $userDetails['county']; ?></div>
+            <div class="name">Sub-county: <?php echo $userDetails['sub_county']; ?></div>
         </div>
+       
         
     </container>
 </body>
