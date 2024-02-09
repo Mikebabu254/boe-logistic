0a0d34ee3b6bc0af39fd10c03b1a7383ed2be669
Theeicon45 <157601461+Theeicon45@users.noreply.github.com>
2024-02-09 12:09:37 +0300

    sender php +css

---
+++ sent.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sent</title>
+    <link rel="stylesheet" href="styles/sent.css">
+
+</head>
+<body>
+    <div class="sent_form">
+        <div class="text"> Sender</div>
+
+        <div class="item_details">
+
+            <input type="text" placeholder="Item Name" name="itemname" class="input" required>
+            <input type="text" placeholder="Item No" name="itemno" class="input" required>
+
+        </div>  
+
+        <input type="text" placeholder="Delivery Agent Name" name="name" class="input" required>
+
+    <div class="date_time"> 
+            <input type="text" placeholder="Date" name="date" class="input" required>
+            <input type="text" placeholder="Time" name="time" class="input" required>
+
+
+    </div>
+    <div class="location">
+            <input type="text" placeholder="County" name="county" class="input" required>
+            <input type="text" placeholder="Sub-county" name="subcounty" class="input" required>
+
+
+    </div>
+
+    </div>
+    
+</body>
+</html>
