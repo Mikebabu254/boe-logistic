28959b7c46519321de30d4af0e0ba58620b4cdb8
Michael ndolo <michaelndolo17142@gmail.com>
2024-02-09 14:02:31 +0300

    remove white space

--- sent.php
+++ sent.php
@@ -9,38 +9,27 @@
 </head>
 <body>
     <div class="sent_form"> 
-            
-
-            <div class="text"> Sender</div>
-            <div class="agentname">
+    
+        <div class="text"> Sender</div>
+        <div class="agentname">
             Delivery Agent Name :
-            </div>
-            <input type="text" placeholder="Item Name" name="itemname" class="itemdet" required>
+        </div>
+        <input type="text" placeholder="Item Name" name="itemname" class="itemdet" required>
             
         <div class="item_details">
-
-            
-           
-            
             <div class="item_no"> Item No: </div>
             <button class="butn">Generate</button>
-
         </div>  
       
-        
-
-    <div class="date_time"> 
+        <div class="date_time"> 
             <input type="date" placeholder="Date" name="date" class="input" required>
             <input type="time" placeholder="Time" name="time" class="input" required>
 
-
-    </div>
-    <div class="location">
+        </div>
+        <div class="location">
             <input type="text" placeholder="County" name="county" class="input" required>
             <input type="text" placeholder="Sub-county" name="subcounty" class="input" required>
-
-
-    </div>
+        </div>
 
     </div>
     
